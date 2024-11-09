ae7171799fc03930143a34e38f9e7170317024d0
maloch18 <mdlochridge@gmail.com>
2024-11-08 18:10:01 -0600

    used fminsearch and lsqnonlin to optimize Boukamp equivalent circuit parameters. Need better initial guesses, and to check the KK equations in the lsqnonlin method

--- fitKK.m
+++ fitKK.m
@@ -1,154 +1,90 @@
-function [] = fitKK(data) % freq, Zdata, Zcircuit, elementVals
+function [Zfit, Zres, Rinf, Rfit, Cfit, R2] = fitKK(data, method) 
 % Fit EIS data by applying Kramers-Kronig relations 
-% to the equivalent circuit impedance function.
+% to an equivalent circuit impedance function.
 
-freq = data.Freq;
-Zreal = data.Zreal;
-Zimag = data.Zimag;
-Zdata = Zreal + 1i*Zimag;
-weights = 1./(data.Zmod.^2);
+    % Initialize data
+    freq = data.Freq;
+    w = 2*pi*freq;
+    N = length(freq);
+    Zdata = data.Zreal + 1i*data.Zimag;
 
-% Assumes a vectorized impedance function that takes arguments as
-% -> Zcircuit([elementVals ',' freq])
+    %% fminsearch: check initial guesses
+    if strcmp(method,'fminsearch')
+        % Kramers-Kronig relations for equivalent circuit (Boukamp, 1995)
+        numVoigt = N;
+        imagSum = @(x,n) sum(x(1:numVoigt)./(1+(w(n)*x(numVoigt+1:end)).^2));
+        % weights = data.Zmod.^(-2);
+        Rinf = data.Zreal(1);
+        ZcirRe = @(x,n) Rinf + imagSum(x,n);
+        ZcirIm = @(x,n) -sum(w(n)*x(1:numVoigt).*x(numVoigt+1:end)./(1+(w(n)*x(numVoigt+1:end)).^2));
+        Zcircuit = @(x,n) ZcirRe(x,n) + 1i*ZcirIm(x,n);
+
+        % Initial guesses
+        R0 = 10^8 * randn(numVoigt,1); % how to optimize guesses?
+        tau0 = 1./freq;
+        x0 = [R0; tau0];
     
-    %{
-    % Kramers-Kronig calculations
-    ZcirPoints = Zcircuit([elementVals ',' freq]);
-    weights = 1./(abs(ZcirPoints).^2);
-    imagIntegral = 2/pi * integral(@(x) (x*imag(Zcircuit(elementVals,x)) - freq.*imag(ZcirPoints))./(x^2-freq.^2), 0, Inf);
-    Rinf = sum(weights.*(real(ZcirPoints)-imagIntegral))/sum(weights);
-    ZfitRe = Rinf + imagIntegral;
-    ZfitIm = 2/pi*freq .* integral(@(x) (real(Zcircuit(elementVals,x) - real(ZcirPoints)))./(x^2-freq.^2), 0, Inf);
-    Zfit = ZfitRe + 1i*ZfitIm;
+        % Simplex method
+        optimFunc = @(x,n) Zcircuit(x,n) - Zdata(n);
+        options = optimset(@fminsearch);
+        xfit = zeros(2*numVoigt,N);
+        Zfit = zeros(N,1);
+        for n = 1:N
+            [x, Zres] = fminsearch(optimFunc,x0,options,n);
+            xfit(:,n) = x;
+            Zfit(n) = Zres;
+        end
+        Rfit = xfit(1:numVoigt)';
+        Cfit = xfit(numVoigt+1:end)';
+        Zfit = Zres + Zdata;
+
+    %% lsqnonlin: check imagSum and ZcirIm
+    elseif strcmp(method,'lsqnonlin')
+        % Kramers-Kronig relations for equivalent circuit (Boukamp, 1995)
+        numVoigt = N;
+        imagSum = @(x) sum(x(1:numVoigt)/(1+(w*x(numVoigt+1:end)).^2));
+        weights = data.Zmod.^(-2);
+        Rinf = data.Zreal(1);
+        ZcirRe = @(x) Rinf + imagSum(x);
+        ZcirIm = @(x) -w*sum(x(1:numVoigt).*x(numVoigt+1:end)/(1+w*x(numVoigt+1:end)).^2);
+        Zcircuit = @(x) ZcirRe(x) + 1i*ZcirIm(x);
+        
+        % Initial guesses
+        R0 = 10^8 * randn(numVoigt,1);
+        tau0 = 1./freq;
+        x0 = [R0; tau0];
+    
+        % Nonlinear least-squares method
+        optimFunc = @(x) Zcircuit(x) - Zdata;
+        xfit = lsqnonlin(optimFunc, x0');
+        Rfit = xfit(1:numVoigt);
+        Cfit = xfit(numVoigt+1:end);
+        Zfit = Zcircuit(xfit);
+        Zres = Zfit - Zdata;
+
+    else
+        disp('Please specify a valid fitting method')
+        return;
+    end
 
     % Residuals
-    ZcirRes = Zdata - ZcirPoints;
-    ZfitRes = Zdata - Zfit;
+    ZmodRes = abs(Zfit) - data.Zmod;
+    ZfitPh = 180/pi*phase(Zfit);
+    ZphRes = ZfitPh - data.Zphz;
 
-    % Goodness of Fit (R^2)
-    R2cirRe = 1 - sum(real(ZcirRes).^2)/sum(real(Zdata-mean(Zdata)).^2);
-    R2fitRe = 1 - sum(real(ZcircFit).^2)/sum(real(Zdata-mean(Zdata)).^2);
-    R2cirIm = 1 - sum(imag(ZcirRes).^2)/sum(imag(Zdata-mean(Zdata)).^2);
-    R2fitIm = 1 - sum(imag(ZcircFit).^2)/sum(imag(Zdata-mean(Zdata)).^2);
-    R2cirMod = 1 - sum(abs(ZcirRes).^2)/sum(abs(Zdata-mean(Zdata)).^2);
-    R2fitMod = 1 - sum(abs(ZcircFit).^2)/sum(abs(Zdata-mean(Zdata)).^2);
-    R2cirPh = 1 - sum(phase(ZcirRes).^2)/sum(phase(Zdata-mean(Zdata)).^2);
-    R2fitPh = 1 - sum(phase(ZcircFit).^2)/sum(phase(Zdata-mean(Zdata)).^2);
-
-    % Nyquist plot
-    figure(1)
-    clf,cla
-    plot(real(Zdata),-imag(Zdata),'ok');
-    hold on
-    plot(real(ZcirPoints),-imag(ZcirPoints),'ob');
-    plot(ZfitRe,-ZfitIm,'-b');
-    hold off
-    xlabel('Re(Z)');
-    ylabel('-Im(Z)');
-    legend('Data','Circuit','Fit','Location','southwest');
-
-    % Bode plot
-    figure(2)
-    clf,cla
-    yyaxis left
-    loglog(freq,abs(ZData),'o');
-    hold on
-    loglog(freq,abs(ZcirPoints),'.');
-    loglog(freq,abs(Zfit),'-');
-    xlim([freq(end),freq(1)]);
-    xlabel('Freq');
-    ylabel('Z');
-    yyaxis right
-    semilogx(freq,phase(Zdata),'o');
-    semilogx(freq,phase(ZcirPoints),'.');
-    semilogx(freq,phase(Zfit),'-');
-    hold off
-    ylabel('Phase');
-    legend('Data','Circuit','Fit','Location','southwest');
-    
-    % Residual plots
-    figure(3)
-    clf,cla
-    subplot(2,2,1)
-    semilogx(freq,real(ZcirRes./Zdata),'.');
-    hold on
-    semilogx(freq,real(ZfitRes./Zdata),'.');
-    hold off
-    xlim([freq(end),freq(1)]);
-    xlabel('Freq');
-    ylabel('Normalized Re(Z) Residuals');
-    legend('Circuit','Fit');
-    title(['Circuit $R_{\mathrm{re}}^2$ = ',num2str(R2cirRe),'; Fit $R_{\mathrm{re}}^2$ = ',num2str(R2fitRe)],'Interpreter','latex');
-    
-    subplot(2,2,2)
-    semilogx(freq,imag(ZcirRes./Zdata),'.');
-    hold on
-    semilogx(freq,imag(ZcirRes./Zdata),'.');
-    hold off
-    xlim([freq(end),freq(1)]);
-    xlabel('Freq');
-    ylabel('Normalized Im(Z) Residuals');
-    legend('Circuit','Fit');
-    title(['Circuit $R_{\mathrm{im}}^2$ = ',num2str(R2cirIm),'; Fit $R_{\mathrm{im}}^2$ = ',num2str(R2fitIm)],'Interpreter','latex');
-    
-    subplot(2,2,3)
-    semilogx(freq,abs(ZcirRes./Zdata),'.');
-    hold on
-    semilogx(freq,abs(ZfitRes./Zdata),'.');
-    hold off
-    xlim([freq(end),freq(1)]);
-    xlabel('Freq');
-    ylabel('Normalized Z Residuals');
-    legend('Circuit','Fit');
-    title(['Circuit $R_{\mathrm{mod}}^2$ = ',num2str(R2cirMod),'; Fit $R_{\mathrm{mod}}^2$ = ',num2str(R2fitMod)],'Interpreter','latex');
-    
-    subplot(2,2,4)
-    semilogx(freq,phase(ZcirRes./Zdata),'.');
-    hold on
-    semilogx(freq,phase(ZfitRes./Zdata),'.');
-    hold off
-    xlim([freq(end),freq(1)]);
-    xlabel('Freq');
-    ylabel('Normalized Phase Residuals');
-    legend('Circuit','Fit');
-    title(['Circuit $R_{\mathrm{ph}}^2$ = ',num2str(R2cirPh),'; Fit $R_{\mathrm{ph}}^2$ = ',num2str(R2fitPh)],'Interpreter','latex');
-    %}
-
-    % Brute force KK fit without equivalent circuit 
-    realIntegrals = zeros(length(freq),1);
-    imagIntegrals = zeros(length(freq),1);
-    for n = 1:length(freq)
-        if n <= 2
-            imagIntegrals(n) = 2/pi * trapz(freq(n+1:end), (freq(n+1:end).*Zimag(n+1:end)-freq(n)*Zimag(n))./(freq(n+1:end).^2-freq(n)^2));
-            realIntegrals(n) = -2/pi * freq(n) * trapz(freq(n+1:end), (Zreal(n+1:end)-Zreal(n))./(freq(n+1:end).^2-freq(n)^2));
-        elseif n >= length(freq)-1
-            imagIntegrals(n) = 2/pi * trapz(freq(1:n-1), (freq(1:n-1).*Zimag(1:n-1)-freq(n)*Zimag(n))./(freq(1:n-1).^2-freq(n)^2));
-            realIntegrals(n) = -2/pi * freq(n) * trapz(freq(1:n-1), (Zreal(1:n-1)-Zreal(n))./(freq(1:n-1).^2-freq(n)^2));
-        else
-            imagIntegrals(n) = 2/pi * ( trapz(freq(1:n-1), (freq(1:n-1).*Zimag(1:n-1)-freq(n)*Zimag(n))./(freq(1:n-1).^2-freq(n)^2)) + trapz(freq(n+1:end), freq(n)./freq(n+1:end).*(Zimag(n+1:end)-Zimag(n))./(freq(n+1:end).^2-freq(n)^2)) );
-            realIntegrals(n) = -2/pi * freq(n) * ( trapz(freq(1:n-1), (Zreal(1:n-1)-Zreal(n))./(freq(1:n-1).^2-freq(n)^2)) + trapz(freq(n+1:end), (Zreal(n+1:end)-Zreal(n))./(freq(n+1:end).^2-freq(n)^2)) );
-        end
-    end
-    
-    Rinf = sum(weights.*(Zreal - imagIntegrals))/(sum(weights));
-    ZrealKK = Rinf + imagIntegrals;
-    ZimagKK = realIntegrals;
-    
-    Zfit = ZrealKK + 1i*ZimagKK;
-    
-    Zres = Zdata-Zfit;
-    
+    % Goodness of fit
     R2real = 1 - sum(real(Zres).^2)/sum(real(Zdata-mean(Zdata)).^2);
     R2imag = 1 - sum(imag(Zres).^2)/sum(imag(Zdata-mean(Zdata)).^2);
-    R2mod = 1 - sum(abs(Zres).^2)/sum(abs(Zdata-mean(Zdata)).^2);
-    R2phase = 1 - sum(phase(Zres).^2)/sum(phase(Zdata-mean(Zdata)).^2);
+    R2mod = 1 - sum(ZmodRes.^2)/sum((data.Zmod-mean(data.Zmod)).^2);
+    R2phase = 1 - sum(ZphRes.^2)/sum(real(data.Zphz-mean(data.Zphz)).^2);
+    R2 = [R2real R2imag R2mod R2phase]';
 
     % Nyquist plot
     figure(1)
     clf,cla
-    plot(real(Zdata),-imag(Zdata),'.');
+    plot(real(Zdata), -imag(Zdata), '.');
     hold on
-    plot(real(Zfit),-imag(Zfit),'-');
+    plot(real(Zfit), -imag(Zfit), '-');
     hold off
     xlabel('Re(Z)');
     ylabel('-Im(Z)');
@@ -158,15 +94,15 @@ weights = 1./(data.Zmod.^2);
     figure(2)
     clf,cla
     yyaxis left
-    loglog(freq,abs(Zdata),'.');
+    loglog(freq, data.Zmod, '.');
     hold on
-    loglog(freq,abs(Zfit),'-');
-    xlim([freq(end),freq(1)]);
+    loglog(freq, abs(Zfit), '-');
+    xlim([freq(end) freq(1)]);
     xlabel('Freq');
     ylabel('Z');
     yyaxis right
-    semilogx(freq,phase(Zdata),'.');
-    semilogx(freq,phase(Zfit),'-');
+    semilogx(freq, data.Zphz, '.');
+    semilogx(freq, ZfitPh, '-');
     hold off
     ylabel('Phase');
     legend('Data','Fit','Location','southwest');
@@ -175,31 +111,31 @@ weights = 1./(data.Zmod.^2);
     figure(3)
     clf,cla
     subplot(2,2,1)
-    semilogx(freq,real(Zres./Zdata),'.');
-    xlim([freq(end),freq(1)]);
+    semilogx(freq, real(Zres)./real(Zfit), '.');
+    xlim([freq(end) freq(1)]);
     xlabel('Freq');
     ylabel('Normalized Re(Z) Residuals');
-    title(['$R_{\mathrm{re}}^2$ = ',num2str(R2real)],'Interpreter','latex');
+    title(['$R^2_{\mathrm{re}}$ = ' num2str(R2real)],'Interpreter','latex');
     
     subplot(2,2,2)
-    semilogx(freq,imag(Zres./Zdata),'.');
-    xlim([freq(end),freq(1)]);
+    semilogx(freq, imag(Zres)./imag(Zfit), '.');
+    xlim([freq(end) freq(1)]);
     xlabel('Freq');
     ylabel('Normalized Im(Z) Residuals');
-    title(['$R_{\mathrm{im}}^2$ = ',num2str(R2imag)],'Interpreter','latex');
-    
+    title(['$R^2_{\mathrm{im}}$ = ' num2str(R2imag)],'Interpreter','latex');
+
     subplot(2,2,3)
-    semilogx(freq,abs(Zres./Zdata),'.');
-    xlim([freq(end),freq(1)]);
+    semilogx(freq, ZmodRes./abs(Zfit), '.');
+    xlim([freq(end) freq(1)]);
     xlabel('Freq');
     ylabel('Normalized Z Residuals');
-    title(['$R_{\mathrm{mod}}^2$ = ',num2str(R2mod)],'Interpreter','latex');
-    
+    title(['$R^2_{\mathrm{mod}}$ = ' num2str(R2mod)],'Interpreter','latex');
+
     subplot(2,2,4)
-    semilogx(freq,phase(Zres./Zdata),'.');
-    xlim([freq(end),freq(1)]);
+    semilogx(freq, ZphRes./ZfitPh, '.');
+    xlim([freq(end) freq(1)]);
     xlabel('Freq');
     ylabel('Normalized Phase Residuals');
-    title(['$R_{\mathrm{ph}}^2$ = ',num2str(R2phase)],'Interpreter','latex');
+    title(['$R^2_{\mathrm{ph}}$ = ' num2str(R2phase)],'Interpreter','latex');
 
 end
